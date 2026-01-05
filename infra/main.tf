003c8354d8de9ac7e879d58b10970c647017a95f
Dxme98 <dominik-bleser@web.de>
2026-01-05 17:24:57 +0100

    fixxed terraform java dynamodb table name missmatch

--- infra/main.tf
+++ infra/main.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_public_access_block" "allow_public" {
 }
 
 resource "aws_dynamodb_table" "file_metadata" {
-  name           = "file_metadata"
+  name           = "FileMetadata"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "fileId"
 
@@ -79,7 +79,7 @@ resource "aws_dynamodb_table" "folder_shares" {
 
 
 resource "aws_dynamodb_table" "folder" {
-  name           = "folder"
+  name           = "Folder"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "folderId"
 
